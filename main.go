48ddc7201e23a1f71299b2efb94e1beae7fe76ed
JING DONG <me@jing.io>
2016-08-26 00:31:27 +0100

    Writing message to pubsub now

--- main.go
+++ main.go
@@ -3,19 +3,48 @@ package main
 import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"github.com/labstack/echo/engine/standard"
+	"golang.org/x/net/context"
+	"log"
+	"cloud.google.com/go/pubsub"
+	"io/ioutil"
 	"net/http"
+	"github.com/labstack/echo/engine/standard"
+	"fmt"
+)
+
+const(
+	topicName = "events"
+	projectId = "development-50atoms"
 )
 
 func main() {
+	ctx := context.Background()
+
+	client, err := pubsub.NewClient(ctx, projectId)
+	if err != nil {
+		log.Fatalf("creating pubsub client: %v", err)
+	}
+	topic := client.Topic(topicName)
 
 	app := echo.New()
 	app.Use(middleware.Logger())
 	app.Use(middleware.Recover())
 	app.Post("/event", func(c echo.Context) error {
-		//
-		c.Logger().Info("message received, sending to pubsub");
-		return c.JSON(http.StatusAccepted, make(map[string]string))
+
+		msg, err := ioutil.ReadAll(c.Request().Body())
+		if err != nil {
+			return 	c.JSON(http.StatusAccepted, map[string]string {
+				"error": "invalid message",
+			})
+		} else {
+			msgID, err := topic.Publish(ctx, &pubsub.Message{
+				Data: msg,
+			})
+			fmt.Print(err)
+			fmt.Println(msgID)
+
+			return c.JSON(http.StatusAccepted, make(map[string]string))
+		}
 	})
 	app.Run(standard.New(":3000"))
 }
